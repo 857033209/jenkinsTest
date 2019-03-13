4a57034176db20855d80513487027b92f6fe7ffe
QuinllunLi <857033209@qq.com>
2019-03-13 23:05:50 +0800

    新建

---
+++ jenkinsTest/Assets/test1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test1 : MonoBehaviour {
+
+    public Text text1;
+    public InputField input;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text1.text = input.text;
+
+    }
+
+}
